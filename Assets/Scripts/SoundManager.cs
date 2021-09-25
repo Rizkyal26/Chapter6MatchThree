1b4bdba8df514925532b87b5a99bb1c5bb9a12af
Rizky Alfiansyah <Ralfiansyah111@gmail.com>
2021-09-25 21:02:56 +0700

    Chapter 6 Match three

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    #region Singleton
+
+    private static SoundManager _instance = null;
+
+    public static SoundManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<SoundManager>();
+
+                if (_instance == null)
+                {
+                    Debug.LogError("Fatal Error: TimeManager not Found");
+                }
+            }
+
+            return _instance;
+        }
+    }
+
+    #endregion
+
+    public AudioClip scoreNormal;
+    public AudioClip scoreNormal2;
+    public AudioClip scoreCombo;
+    public AudioClip scoreCombo2;
+
+
+    public AudioClip wrongMove;
+
+    public AudioClip tap;
+
+    private AudioSource player;
+
+    private void Start()
+    {
+        player = GetComponent<AudioSource>();
+    }
+
+    public void PlayScore(bool isCombo)
+    {
+        if (isCombo)
+        {
+            player.PlayOneShot(scoreCombo);
+            player.PlayOneShot(scoreCombo2);
+        }
+        else
+        {
+            player.PlayOneShot(scoreNormal);
+            player.PlayOneShot(scoreNormal2);
+        }
+    }
+
+    public void PlayWrong()
+    {
+        player.PlayOneShot(wrongMove);
+    }
+
+    public void PlayTap()
+    {
+        player.PlayOneShot(tap);
+    }
+}
---
+++ ReadMe.txt
@@ -0,0 +1,8 @@
+Match Three
+mainkan dan rasakan game bernostalgia seperti Bejeweld. bersainglah bersama teman mu untuk meraih skor tertinggi dalam waktu 2 menit.
+
+update:
+(+) Menambahkan Sfx Score
+(+) Menambahkan Sfx Combo
+(+) Menambahkan BGM untuk pengalaman bermain yang lebih baik
+(+) Menambahkan Sfx GameOver
